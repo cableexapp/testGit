57e0039b13cb58b65cacfc76a824abfbfe14f463
App01 <app01@App01deiMac.local>
2014-09-22 11:06:45 +0800

    cx

--- test/test/AppDelegate.m
+++ test/test/AppDelegate.m
@@ -15,6 +15,7 @@
     NSLog(@"ttttt");
     
     
+    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
